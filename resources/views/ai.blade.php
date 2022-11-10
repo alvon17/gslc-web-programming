4398d6cb4c27395d9689df2c18db54045d118ef5
alvon17 <73384433+alvon17@users.noreply.github.com>
2022-11-10 17:21:36 +0700

    update gslc2

--- resources/views/ai.blade.php
+++ resources/views/ai.blade.php
@@ -2,56 +2,65 @@
 @section('title', 'Class AI')
 
 @section('content')
-    <table class="table">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Age</th>
-                <th scope="col">Ipk</th>
-                <th scope="col">Status</th>
-            </tr>
-        </thead>
-        <tbody>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-2 g-3 left-side">
+                <h1 class="up-to-bottom-text">@yield('title')</h1>
+            </div>
+            <div class="col-10">
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Age</th>
+                            <th scope="col">Ipk</th>
+                            <th scope="col">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-        </tbody>
-        <tr>
-            <td>
-                @forelse ($studentName as $name)
-                    <p>{{ $name }}</p>
-                @empty
-                    <p>No Student</p>
-                @endforelse
-            </td>
-            <td>
-                @for ($i = 0; $i < count($studentAge); $i++)
-                    <p>{{ $studentAge[$i] }}</p>
-                @endfor
-            </td>
-            <td>
-                @php
-                    $i = 0;
-                @endphp
-                @while ($i < count($studentIpk))
-                    <p>{{ $studentIpk[$i] }}</p>
-                    @php
-                        $i++;
-                    @endphp
-                @endwhile
-            </td>
-            <td>
-                @foreach ($studentIpk as $ipk)
-                    @if ($ipk >= 3)
-                        <div class="alert alert-success">
-                            Lulus
-                        </div>
-                    @elseif ($ipk < 3)
-                        <div class="alert alert-danger">
-                            Tidak Lulus
-                        </div>
-                    @endif
-                @endforeach
-            </td>
-        </tr>
-        </tbody>
-    </table>
+                    </tbody>
+                    <tr>
+                        <td>
+                            @forelse ($studentName as $name)
+                                <p>{{ $name }}</p>
+                            @empty
+                                <p>No Student</p>
+                            @endforelse
+                        </td>
+                        <td>
+                            @for ($i = 0; $i < count($studentAge); $i++)
+                                <p>{{ $studentAge[$i] }}</p>
+                            @endfor
+                        </td>
+                        <td>
+                            @php
+                                $i = 0;
+                            @endphp
+                            @while ($i < count($studentIpk))
+                                <p>{{ $studentIpk[$i] }}</p>
+                                @php
+                                    $i++;
+                                @endphp
+                            @endwhile
+                        </td>
+                        <td>
+                            @foreach ($studentIpk as $ipk)
+                                @if ($ipk >= 3)
+                                    <div class="alert alert-success">
+                                        Lulus
+                                    </div>
+                                @elseif ($ipk < 3)
+                                    <div class="alert alert-danger">
+                                        Tidak Lulus
+                                    </div>
+                                @endif
+                            @endforeach
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,56 +2,65 @@
 @section('title', 'All Class')
 
 @section('content')
-    <form class="row g-3 search" action="" method="GET">
-        <div class="col-auto">
-            <input type="search" class="form-control name" placeholder="Search Name" name="name">
-        </div>
-        <div class="col-auto">
-            <button type="submit" class="btn btn-primary mb-3 btn-confirm">Confirm identity</button>
-        </div>
-    </form>
-    <table class="table">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Age</th>
-                <th scope="col">Speciality</th>
-                <th scope="col">Campus Region</th>
-            </tr>
-        </thead>
-        <tbody>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-2 g-3 left-side">
+                <h1 class="up-to-bottom-text">@yield('title')</h1>
+            </div>
+            <div class="col-10">
+                <form class="row g-3 search" action="" method="GET">
+                    <div class="col-auto">
+                        <input type="search" class="form-control name" placeholder="Search Name" name="name">
+                    </div>
+                    <div class="col-auto">
+                        <button type="submit" class="btn btn-primary mb-3 btn-confirm">Confirm identity</button>
+                    </div>
+                </form>
+                <table class="table">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Age</th>
+                            <th scope="col">Speciality</th>
+                            <th scope="col">Campus Region</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
-        </tbody>
-        <tr>
-            <td>
-                @forelse ($studentName as $name)
-                    <p>{{ $name }}</p>
-                @empty
-                    <p>No Student</p>
-                @endforelse
-            </td>
-            <td>
-                @foreach ($studentAge as $age)
-                    <p>{{ $age }}</p>
-                @endforeach
-            </td>
-            <td>
-                @for ($i = 0; $i < count($studentSpeciality); $i++)
-                    <p>{{ $studentSpeciality[$i] }}</p>
-                @endfor
-            </td>
-            <td>
-                @php
-                    $i = 0;
-                @endphp
-                @while ($i < count($studentCampus))
-                    <p>{{ $studentCampus[$i] }}</p>
-                    @php
-                        $i++;
-                    @endphp
-                @endwhile
-            </td>
-        </tr>
-        </tbody>
-    </table>
+                    </tbody>
+                    <tr>
+                        <td>
+                            @forelse ($studentName as $name)
+                                <p>{{ $name }}</p>
+                            @empty
+                                <p>No Student</p>
+                            @endforelse
+                        </td>
+                        <td>
+                            @foreach ($studentAge as $age)
+                                <p>{{ $age }}</p>
+                            @endforeach
+                        </td>
+                        <td>
+                            @for ($i = 0; $i < count($studentSpeciality); $i++)
+                                <p>{{ $studentSpeciality[$i] }}</p>
+                            @endfor
+                        </td>
+                        <td>
+                            @php
+                                $i = 0;
+                            @endphp
+                            @while ($i < count($studentCampus))
+                                <p>{{ $studentCampus[$i] }}</p>
+                                @php
+                                    $i++;
+                                @endphp
+                            @endwhile
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
